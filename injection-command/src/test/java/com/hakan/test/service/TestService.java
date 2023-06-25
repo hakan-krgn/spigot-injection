81a753601dc9eee0b50ca7758cb04ef12014b94d
Hakan Kargın <hakankargin03@gmail.com>
2023-06-25 11:08:34 +0300

    reformat codes and optimize imports

--- injection-command/src/test/java/com/hakan/test/service/TestService.java
+++ injection-command/src/test/java/com/hakan/test/service/TestService.java
@@ -1,7 +1,6 @@
 package com.hakan.test.service;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.hakan.injection.annotations.Service;
 import org.bukkit.entity.Player;
 
--- injection-database-hibernate/src/test/java/com/hakan/test/service/TestService.java
+++ injection-database-hibernate/src/test/java/com/hakan/test/service/TestService.java
@@ -1,7 +1,6 @@
 package com.hakan.test.service;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.hakan.injection.annotations.Service;
 import com.hakan.test.model.User;
 import com.hakan.test.repository.TestRepository;
