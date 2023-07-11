0d8cd74c52072469ee1feac391f4717fb828fd24
Hakan Kargın <hakankargin03@gmail.com>
2023-07-11 15:50:29 +0300

    add missing javadocs

--- injection-core/src/main/java/com/hakan/spinjection/annotations/ExecutorOrder.java
+++ injection-core/src/main/java/com/hakan/spinjection/annotations/ExecutorOrder.java
@@ -5,9 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * ExecutorOrder annotation to define
+ * executor order of the spigot modules.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExecutorOrder {
 
+    /**
+     * Order of the executor.
+     * If the value is smaller, the
+     * executor will be executed first.
+     *
+     * @return Order of the executor.
+     */
     int value();
 }
