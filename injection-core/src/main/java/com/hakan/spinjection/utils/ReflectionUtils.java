0e3ab400973e46192a850775710398e48d2e6767
Hakan Kargın <hakankargin03@gmail.com>
2023-07-17 17:54:28 +0300

    remove unnecessary creation methods from ReflectionUtils

--- injection-core/src/main/java/com/hakan/spinjection/utils/ReflectionUtils.java
+++ injection-core/src/main/java/com/hakan/spinjection/utils/ReflectionUtils.java
@@ -1,8 +1,6 @@
 package com.hakan.spinjection.utils;
 
-import com.hakan.basicdi.reflection.Reflection;
 import com.hakan.spinjection.annotations.Async;
-import com.hakan.spinjection.annotations.Scanner;
 import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -17,59 +15,6 @@ import java.lang.reflect.Method;
  */
 @SuppressWarnings({"unchecked"})
 public class ReflectionUtils {
-
-    /**
-     * Creates a new Reflections instance.
-     *
-     * @param plugin plugin
-     * @return reflections
-     */
-    public static @Nonnull Reflection createFrom(@Nonnull Plugin plugin) {
-        return createFrom(plugin.getClass());
-    }
-
-    /**
-     * Creates a new Reflections instance.
-     *
-     * @param instance instance
-     * @return reflections
-     */
-    public static @Nonnull Reflection createFrom(@Nonnull Object instance) {
-        return createFrom(instance.getClass());
-    }
-
-    /**
-     * Creates a new Reflections instance.
-     *
-     * @param clazz clazz
-     * @return reflections
-     */
-    public static @Nonnull Reflection createFrom(@Nonnull Class<?> clazz) {
-        return createFrom(clazz.getAnnotation(Scanner.class));
-    }
-
-    /**
-     * Creates a new Reflections instance.
-     *
-     * @param scanner scanner
-     * @return reflections
-     */
-    public static @Nonnull Reflection createFrom(@Nonnull Scanner scanner) {
-        return createFrom(scanner.value());
-    }
-
-    /**
-     * Creates a new Reflections instance.
-     *
-     * @param path path
-     * @return reflections
-     */
-    public static @Nonnull Reflection createFrom(@Nonnull String path) {
-        if (path.isEmpty())
-            throw new RuntimeException("scan path cannot be empty!");
-
-        return new Reflection(path);
-    }
 
     /**
      * Gets field value from the instance.
